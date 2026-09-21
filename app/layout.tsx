01638fb5ddbf47b77d81ede343a7d92956b346db
Gaurav <gauravbhardwaj7489@gmail.com>
2026-09-22 00:38:53 +0530

    style: load v10 inner-page system after legacy layers

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import "./pro-upgrade.css";
 import "./design-v8.css";
 import "./inner-pages-v9.css";
 import "./responsive-fixes.css";
+import "./inner-pages-v10.css";
 import { SiteHeader } from "@/components/site-header";
 import { SiteFooter } from "@/components/site-footer";
 import { InteractiveLayer } from "@/components/interactive";
