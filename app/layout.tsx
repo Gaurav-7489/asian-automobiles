e2f6e51fdbdf1c4db4a394eb751fa71fc5ba7c8b
Gaurav <gauravbhardwaj7489@gmail.com>
2026-09-22 11:21:49 +0530

    style: load v13 contrast and page polish last

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,7 @@ import "./responsive-fixes.css";
 import "./inner-pages-v10.css";
 import "./inner-pages-v11.css";
 import "./responsive-contrast-v12.css";
+import "./inner-pages-v13.css";
 import { SiteHeader } from "@/components/site-header";
 import { SiteFooter } from "@/components/site-footer";
 import { InteractiveLayer } from "@/components/interactive";
